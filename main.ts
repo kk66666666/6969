9369d63a8888077e305f8f1d32fae6bcab258140
kk66666666 <115142452+kk66666666@users.noreply.github.com>
2023-04-20 15:12:32 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,40 @@
-input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-    sprite = 1
-})
 let _this: game.LedSprite = null
-let sprite = 0
-input.setSoundThreshold(SoundThreshold.Loud, 140)
 let main = game.createSprite(2, 2)
+let lose = 0
+let score = 0
+game.setLife(5)
 loops.everyInterval(1000, function () {
-    game.setScore(Math.trunc(input.runningTime() / 10000))
-    if (input.lightLevel() < 160) {
-        main.change(LedSpriteProperty.Y, -1)
-        basic.pause(500)
-    } else if (50 < input.acceleration(Dimension.Strength)) {
-        main.change(LedSpriteProperty.Y, 1)
+    if (main.isTouchingEdge()) {
+        game.removeLife(1)
         basic.pause(500)
     }
-    if (1 == sprite) {
-        main.change(LedSpriteProperty.X, -1)
+})
+basic.forever(function () {
+    if (lose == 5) {
+        game.setScore(score)
+        game.gameOver()
+    }
+})
+loops.everyInterval(200, function () {
+    if (input.lightLevel() < 50) {
+        main.change(LedSpriteProperty.Y, -1)
         basic.pause(500)
-    } else {
-        main.change(LedSpriteProperty.X, 1)
+    } else if (60 < input.acceleration(Dimension.Strength)) {
+        main.change(LedSpriteProperty.Y, 1)
         basic.pause(500)
     }
 })
 loops.everyInterval(input.temperature() * 100, function () {
     _this = game.createSprite(4, randint(0, 4))
     for (let index = 0; index < 4; index++) {
-        basic.pause(randint(200, 500))
+        basic.pause(randint(50, 200))
         _this.change(LedSpriteProperty.X, -1)
         if (_this.isTouching(main)) {
-            game.gameOver()
+            game.removeLife(1)
+            basic.pause(500)
         }
     }
     basic.pause(100)
     _this.delete()
+    score += 1
 })
